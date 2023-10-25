0047e59b0850f2da69389699a2fb9b0518185d53
Davide Filippi <d91.filippi@gmail.com>
2023-10-25 17:49:35 +0200

    add sistemjs single-spa and import-map-overrides

--- child12/src/app/app-routing.module.ts
+++ child12/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
-const routes: Routes = [];
+const routes: Routes = [/*{
+  path: 'child2',
+  children: [
+    /**
+     * All routes specific to this app go here.
+     */
+    /*{
+      path: 'page1',
+      component: Page1Component
+    }, {
+      path: 'page2',
+      component: Page2Component
+    }]
+}, */{
+  path: '**',
+  component: EmptyRouteComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/child12' }],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- container/src/app/app-routing.module.ts
+++ container/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'child12',
+  /*children: [{
+    path: '**',
+    loadChildren: () => import('./spa-host/spa-host.module').then(m => m.SpaHostModule),
+    data: { app: 'child1' }
+  }]
+}, {
+  path: 'child2',
+  children: [{
+    path: '**',
+    loadChildren: () => import('./spa-host/spa-host.module').then(m => m.SpaHostModule),
+    data: { app: 'child2' }
+  }]*/
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
