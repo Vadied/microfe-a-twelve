e77735f23eb95b75a76d75064d6bb3203d2ed3bb
Davide Filippi <d91.filippi@gmail.com>
2023-10-26 10:52:53 +0200

    single-spa working

--- container/src/app/app-routing.module.ts
+++ container/src/app/app-routing.module.ts
@@ -1,24 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
-  path: 'child12',
-  /*children: [{
-    path: '**',
-    loadChildren: () => import('./spa-host/spa-host.module').then(m => m.SpaHostModule),
-    data: { app: 'child1' }
-  }]
-}, {
-  path: 'child2',
-  children: [{
-    path: '**',
-    loadChildren: () => import('./spa-host/spa-host.module').then(m => m.SpaHostModule),
-    data: { app: 'child2' }
-  }]*/
-}];
+const routes: Routes = [
+  {
+    path: 'child12',
+    children: [
+      {
+        path: '**',
+        loadChildren: () =>
+          import('./spa-host/spa-host.module').then((m) => m.SpaHostModule),
+        data: { app: 'child12' },
+      },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
